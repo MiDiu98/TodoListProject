cf4e5d7834abb388c422ecea22681b6c92b1eb83
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-04-18 00:34:17 +0700

    Fix 9

--- todolist-test.sql
+++ todolist-test.sql
@@ -42,8 +42,7 @@ CREATE TABLE IF NOT EXISTS `todolist-test`.`task` (
                                                       `title` VARCHAR(255) NULL DEFAULT NULL,
                                                       PRIMARY KEY (`id`))
     ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
+    DEFAULT CHARACTER SET = utf8mb4;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
